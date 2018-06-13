d76af5f56ae94b3ca519836f88da2224cc5c37bc
angeloortega <angeloale1@gmail.com>
2018-06-13 01:31:24 -0600

    Generacion de Calendario

---
+++ Fifa19/Fifa19/Models/Calendar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fifa19.Models
+{
+    public class Calendar
+    {
+        public decimal idCompeticion { get; set; }
+        public decimal anho { get; set; }
+        public System.DateTime fchInicio { get; set; }
+    }
+}
